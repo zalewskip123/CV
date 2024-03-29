be75e8abea705732b8721d64aed30ac81c536393
Piotr Zalewski <zalewskipiotr050@gmail.com>
2024-03-29 12:44:58 +0100

    Update CV Project 29.03.2024

--- src/components/layout/PersonalData/PersonalData.js
+++ src/components/layout/PersonalData/PersonalData.js
@@ -7,12 +7,21 @@ function PersonalData() {
             <p className="PersonalDataTitle">Dane osobowe</p>
             <hr />
             <div className="PersonalDataList">
-                <div className="PersonalDataName"><p>Numer telefonu</p></div>
-                <div className="PersonalDataValue"><p>514684032</p></div>
-                <div className="PersonalDataName"><p>Adres Email</p></div>
-                <div className="PersonalDataValue"><p>zalewskipiotr050@gmail.com</p></div>
-                <div className="PersonalDataName"><p>Miejsce zamieszkania</p></div>
-                <div className="PersonalDataValue"><p>ul. Ceynowy 2, 84-207 Bojano</p></div>
+                <div className="PersonalDataListLeftSide">
+                    <div className="PersonalDataName"><p>Numer telefonu</p></div>
+                    <div className="PersonalDataValue"><p>514684032</p></div>
+                    <div className="PersonalDataName"><p>Adres Email</p></div>
+                    <div className="PersonalDataValue"><p>zalewskipiotr050@gmail.com</p></div>
+                    <div className="PersonalDataName"><p>Miejsce zamieszkania</p></div>
+                    <div className="PersonalDataValue"><p>ul. Ceynowy 2, 84-207 Bojano</p></div>
+                </div>
+                <div className="PersonalDataListRightSide">
+                    <div className="PersonalDataLinks">
+                        <p className="Title">Linki:</p>
+                        <p className="Links"><a href="https://www.linkedin.com/in/piotr-zalewski-0a32922bb/" target="_blank" rel="noreferrer">LinkedIn</a></p>
+                        <p className="Links"><a href="https://github.com/zalewskip123" target="_blank" rel="noreferrer">GitHub</a></p>
+                    </div>
+                </div>
             </div>
         </div>
     );
